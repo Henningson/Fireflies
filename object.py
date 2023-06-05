466264a56b7ce1b13326966e64e0ab25d0636d96
Henningson <jann-ole.henningson@fau.de>
2023-06-05 10:20:10 +0200

    Added relative object class for structured light projector

--- object.py
+++ object.py
@@ -4,11 +4,15 @@ import utils_math
 import utils_torch
 import transforms_torch
 import random
+from typing import List
 
-class Object:
+class RandomizableObject:
     def __init__(self, file_path: str, device: torch.cuda.device = torch.device("cuda")):
         self._device = device
         self.loadObject(file_path)
+
+        self._last_rotation = None
+        self._last_translation = None
 
     def setRotations(self, 
                      pitch_min: float, 
@@ -76,7 +80,8 @@ class Object:
         pitch_mat = utils_math.getPitchTransform(pitch, self._device)
         roll_mat  = utils_math.getRollTransform(roll, self._device)
 
-        return yaw_mat @ pitch_mat @ roll_mat
+        self._last_rotation = yaw_mat @ pitch_mat @ roll_mat
+        return self._last_rotation
 
     def sampleTranslation(self) -> torch.tensor:
         translationMatrix = torch.eye(4, device=self._device)
@@ -85,6 +90,7 @@ class Object:
         translationMatrix[0, 3] = random_translation[0]
         translationMatrix[1, 3] = random_translation[1]
         translationMatrix[2, 3] = random_translation[2]
+        self._last_translation = translationMatrix
         return translationMatrix
 
     def sampleAnimation(self) -> torch.tensor:
@@ -114,14 +120,138 @@ class Object:
         # Translate Object
         temp_vertex = transforms_torch.transform_points(temp_vertex, self.sampleTranslation())
 
+        return temp_vertex
+
+    def rotation(self) -> torch.tensor:
+        return self._last_rotation
+    
+    def translation(self) -> torch.tensor:
+        return self._last_translation
+
+
+
+
+
+
+class RelativeObject:
+    def __init__(self, file_path: str, parent: RandomizableObject, device: torch.cuda.device = torch.device("cuda")):
+        self._device = device
+        self.loadObject(file_path)
+        self._parent = parent
+
+    def setRotation(self, 
+                     pitch: float, 
+                     yaw: float,
+                     roll: float) -> None:
+        self._pitch = pitch
+        self._yaw   = yaw
+        self._roll  = roll
+
+        yaw_mat   = utils_math.getYawTransform(yaw, self._device)
+        pitch_mat = utils_math.getPitchTransform(pitch, self._device)
+        roll_mat  = utils_math.getRollTransform(roll, self._device)
+
+        self._rotation = yaw_mat @ pitch_mat @ roll_mat
+
+    def setTranslation(self, vec: List[float]) -> None:
+        self._translation = torch.tensor(vec, device=self._device)
+
+    def setScale(self, vec: List[float]) -> None:
+        self._scale = torch.tensor(vec, device=self._device)
+
+    def setAnimation(self,
+                    vertex_offsets: torch.tensor,
+                    replace_vertices: bool = False) -> None:
+        self._vertex_offsets = vertex_offsets.to(self._device)
+        self._replace_vertices = replace_vertices
+
+    def setVertices(self, vertices: List[List[float]]) -> None:
+        self._vertices = torch.tensor(vertices, device=self._device)
+
+    def loadObject(self, file_path: str) -> None:
+        object_dict = utils_io.read_config_yaml(file_path)
+        self.setVertices(object_dict["vertices"])
+        self.setAnimation(object_dict["vertex_offsets"]), bool(object_dict["replace_vertices"])
+        self.setScale(object_dict["scale"])
+        self.setTranslation(object_dict["translation"])
+        self.setRotation(float(object_dict["pitch"]),
+                          float(object_dict["yaw"]),
+                          float(object_dict["roll"]))
+    
+    def sampleScale(self) -> torch.tensor:
+        scaleMatrix = torch.eye(4, device=self._device)
+        random_translation = utils_torch.randomBetweenTensors(self.min_scale, self.max_scale)
+
+        scaleMatrix[0, 0] = random_translation[0]
+        scaleMatrix[1, 1] = random_translation[1]
+        scaleMatrix[2, 2] = random_translation[2]
+        return scaleMatrix
+
+    def sampleRotations(self) -> torch.tensor:
+        pitch = utils_math.uniformBetweenValues(self.pitch_min, self.pitch_max)
+        yaw = utils_math.uniformBetweenValues(self.yaw_min, self.yaw_max)
+        roll = utils_math.uniformBetweenValues(self.roll_min, self.roll_max)
+
+        return self._last_rotation
+
+    def sampleTranslation(self) -> torch.tensor:
+        translationMatrix = torch.eye(4, device=self._device)
+        random_translation = utils_torch.randomBetweenTensors(self.min_translation, self.max_translation)
+
+        translationMatrix[0, 3] = random_translation[0]
+        translationMatrix[1, 3] = random_translation[1]
+        translationMatrix[2, 3] = random_translation[2]
+        self._last_translation = translationMatrix
+        return translationMatrix
+
+    def sampleAnimation(self) -> torch.tensor:
+        if self._vertex_offsets is None:
+            return self._vertices
+
+        num_anim_frames = self._vertex_offsets.shape[0]
+        rand_index = random.randint(0, num_anim_frames - 1)
+
+        if self._replace_vertices:
+            return self._vertex_offsets[rand_index]
+        
+        return self._vertices + self._vertex_offsets[rand_index]
+
+    def getVertexData(self) -> torch.tensor:
+        # Sample Animations
+        temp_vertex = self._verticses
+
+        # Scale Object
+        temp_vertex = transforms_torch.transform_points(temp_vertex, self._scale)
+
+        # Rotate Object
+        rotMat = self._rotation
+        rotMat = transforms_torch.toMat4x4(rotMat)
+        temp_vertex = transforms_torch.transform_points(temp_vertex, rotMat)
+
+        # Translate Object
+        temp_vertex = transforms_torch.transform_points(temp_vertex, self._translation)
+
+        # Rotate by parent
+        p_rotMat = self._parent.rotation()
+        p_rotMat = transforms_torch.toMat4x4(p_rotMat)
+        temp_vertex = transforms_torch.transform_points(temp_vertex, p_rotMat)
+
+        # Translate by parent
+        temp_vertex = transforms_torch.transform_points(temp_vertex, self._parent.translation())
 
         return temp_vertex
 
 
+    def rotation(self) -> torch.tensor:
+        return self._last_rotation
+    
+
+    def translation(self) -> torch.tensor:
+        return self._last_translation
 
 
 def _test():
-    test_obj = Object("test.yaml")
+    test_obj = RandomizableObject("test.yaml")
     test_obj.sampleAnimation()
     test_obj.sampleScale()
     test_obj.sampleTranslation()
